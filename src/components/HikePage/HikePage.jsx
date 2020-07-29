b61a390782c48c4af45e441e331ddc47a7fb6c48
johnrobertmahlan <johnrobertmahlan@gmail.com>
2020-07-28 20:22:46 -0600

    initialize styles for hikepage component

--- src/components/HikePage/HikePage.jsx
+++ src/components/HikePage/HikePage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Hikes from '../Hikes/Hikes';
+import styles from './HikePage.module.css';
 
 const HikePage = (props) => {
     console.log(props.trails);
     if(props.trails.length) return(
-        <div>
+        <div className={styles.trailDisplay}>
             {props.trails.map((trail, idx) => <Hikes trail={ trail } idx={ idx } />)}
         </div>
     ); else return <div>BOO</div>
--- src/components/Hikes/Hikes.jsx
+++ src/components/Hikes/Hikes.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-//import styles from './Hike.module.css';
+import styles from './Hikes.module.css';
 
 const Hikes = (props) => {
     if(props.trail.imgSmallMed !== '') {
         return(
             <div>
-                <div>
+                <div className={styles.trails}>
                     <img src={props.trail.imgSmallMed} alt="Trail"></img>
                     <h3>{props.trail.name}</h3>
                     <h5>{props.trail.location}</h5>
@@ -17,7 +17,7 @@ const Hikes = (props) => {
     } else {
         return(
             <div>
-                <div style={{backgroundColor: 'green'}}>
+                <div className={styles.trails} style={{backgroundColor: 'green'}}>
                     <h3>{props.trail.name}</h3>
                     <h5>{props.trail.location}</h5>
                     <Link to={{pathname: `/hikes/${props.idx}`, state: {idx: props.idx}}}>Learn More!</Link>
