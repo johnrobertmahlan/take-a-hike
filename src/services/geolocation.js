1546fb85bcefe96bdc6a2cb589ffdbb5ab716a5f
johnrobertmahlan <johnrobertmahlan@gmail.com>
2020-08-05 20:30:22 -0600

    update geolocation function

--- src/services/geolocation.js
+++ src/services/geolocation.js
@@ -1,4 +1,4 @@
 export function getLatLng(city) {
-    return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3130b9f6f3c126f9bc6a4a2c72b3f56c`,
+    return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3130b9f6f3c126f9bc6a4a2c72b3f56c`,
     {mode: 'cors'}).then(response => response.json());
 };
